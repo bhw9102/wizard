dbef1cd1f93ce35ae554edfa26715ce535ca12bd
hyunwoo <bhw9102@gmail.com>
2019-03-27 23:12:04 +0900

    필요한 앱과 static 요소를 추가하다.

--- wizard/settings.py
+++ wizard/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'import_export',
+    'django_cleanup.apps.CleanupConfig',
+    'game',
+    'session',
+    'play',
 ]
 
 MIDDLEWARE = [
@@ -115,6 +120,17 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.1/howto/static-files/
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ('game', os.path.join(BASE_DIR, 'game', 'static')),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, 'media'),
+)
+
